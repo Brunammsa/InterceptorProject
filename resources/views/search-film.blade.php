f2361c4263604942d4a83901d6b7b131eabdd384
Bruna Sa <bruna@Mac-mini-de-Bruna.local>
2023-09-14 17:43:11 -0300

    alterando a responsividade para mobile first, exibindo filmes testes de busca

--- resources/views/search-film.blade.php
+++ resources/views/search-film.blade.php
@@ -6,23 +6,93 @@
     </x-slot> --}}
 
     <div class="py-12">
-        <div class="max-w-5xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100 grid grid-flow-col justify-stretch flex flex-row items-baseline">
-                    <div class=" basis-1/2">
+        <div class="max-w-5xl mx-auto md:px-6 lg:px-8">
+            <div class="bg-zinc-300 dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100 flex justify-start items-baseline">
+                    <div>
                         <select class="form-select rounded me-3" name="countries" id="countries">
-                            <option>Selecione o país de busca</option>
-                            @foreach ($countries as $country)
-                                <option value="{{$country->id}}">{{$country->name}}</option>
-                            @endforeach
+                            <option>Brazil</option>
                         </select>
                     </div>
                     <div>
-                        <x-text-input id="movies-name" class="block mt-1" type="text" name="movies-name" placeholder="Digite aqui o nome do filme" required autofocus/>
-                        <x-input-error :messages="$errors->get('movies-name')" class="mt-2" />
+                        <x-text-input id="movies-name" class="block mt-1 md:w-48 lg:w-96" type="text" name="movies-name" placeholder="Digite o nome do filme" required autofocus/>
                     </div>
-                    <div class="basis-1/4">
-                        <x-primary-button>buscar</x-primary-button>
+                </div>
+                <div class="relative p-6 flex w-full flex-col rounded-xl bg-clip-border text-gray-700 shadow-md grid grid-cols-3 gap-4">
+                    <div>
+                        <div class="block rounded-lg bg-white shadow-lg dark:bg-neutral-700">
+                            <a href="#!">
+                                <img class="rounded-t-lg" src="https://tecdn.b-cdn.net/img/new/standard/nature/184.jpg" alt="" /></a>
+                            <div class="p-6 text-center text-center">
+                                <h5
+                                class="mb-2 text-sm text-center font-medium leading-tight text-neutral-800 dark:text-neutral-50">
+                                2023
+                                </h5>
+                                <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+                                batman
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="block rounded-lg bg-white shadow-lg dark:bg-neutral-700">
+                            <a href="#!">
+                                <img class="rounded-t-lg" src="https://tecdn.b-cdn.net/img/new/standard/nature/184.jpg" alt="" /></a>
+                            <div class="p-6 text-center">
+                                <h5
+                                class="mb-2 text-sm text-center font-medium leading-tight text-neutral-800 dark:text-neutral-50">
+                                2023
+                                </h5>
+                                <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+                                titanic
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="block rounded-lg bg-white shadow-lg dark:bg-neutral-700">
+                            <a href="#!">
+                                <img class="rounded-t-lg" src="https://tecdn.b-cdn.net/img/new/standard/nature/184.jpg" alt="" /></a>
+                            <div class="p-6 text-center">
+                                <h5
+                                class="mb-2 text-sm text-center font-medium leading-tight text-neutral-800 dark:text-neutral-50">
+                                2023
+                                </h5>
+                                <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+                                avengers
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="block rounded-lg bg-white shadow-lg dark:bg-neutral-700">
+                            <a href="#!">
+                                <img class="rounded-t-lg" src="https://tecdn.b-cdn.net/img/new/standard/nature/184.jpg" alt="" /></a>
+                            <div class="p-6 text-center">
+                                <h5
+                                class="mb-2 text-sm text-center font-medium leading-tight text-neutral-800 dark:text-neutral-50">
+                                2023
+                                </h5>
+                                <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+                                pinoquio
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="block rounded-lg bg-white shadow-lg dark:bg-neutral-700">
+                            <a href="#!">
+                                <img class="rounded-t-lg" src="https://tecdn.b-cdn.net/img/new/standard/nature/184.jpg" alt="" /></a>
+                            <div class="p-6 text-center">
+                                <h5
+                                class="mb-2 text-sm text-center font-medium leading-tight text-neutral-800 dark:text-neutral-50">
+                                2023
+                                </h5>
+                                <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+                                perdida
+                                </p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
