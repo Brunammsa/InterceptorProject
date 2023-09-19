ce76cdb395f4e262260ed08b30195991db0a68c8
Bruna Sa <bruna@Mac-mini-de-Bruna.local>
2023-09-19 11:12:09 -0300

    parte de imagem, descrição e créditos ok responsivo

---
+++ resources/views/index-movie.blade.php
@@ -0,0 +1,35 @@
+<x-app-layout>
+    <div class=" py-12">
+        <div class=" max-w-5xl mx-auto px-5 md:px-6 lg:px-8">
+            <div class="bg-zinc-300 overflow-hidden shadow-md rounded-lg">
+                <div class="p-6 text-gray-900">
+                    <div class="flex flex-col md:flex-row lg:flex-row md:gap-10 lg:gap-20 items-center">
+                        <div class="text-center">
+                            <img class="mb-3" width="200" height="200"
+                                src="https://tecdn.b-cdn.net/img/new/standard/nature/184.jpg"/></a>
+                            <label for="">1989</label>
+                        </div>
+                        <div class="">
+                            <h1 class="mb-2 text-center">O retorno do batman</h1>
+                            <p class="text-justify w-64">Era uma vez um menino franzino, franziiino... Que viva aos arredores de Ohio...</p>
+                        </div>
+                    </div>
+                    <div class="flex sm:flex-wrap text-sm text-center justify-around gap-4 mt-5 lg:text-base">
+                        <div>
+                            <h1>Fulano de tal</h1>
+                            <p>Bruce Wayne</p>
+                        </div>
+                        <div>
+                            <h1>Fulano de tal</h1>
+                            <p>Bruce Wayne</p>
+                        </div>
+                        <div>
+                            <h1>Fulano de tal</h1>
+                            <p>Bruce Wayne</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
