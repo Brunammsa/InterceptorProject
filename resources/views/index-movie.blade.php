1e9000bf15e3b85318879b4efcbbf4cd01d83464
Bruna Sa <bruna@Mac-mini-de-Bruna.local>
2023-09-19 14:34:00 -0300

    concluindo index filme

--- resources/views/index-movie.blade.php
+++ resources/views/index-movie.blade.php
@@ -7,11 +7,10 @@
                         <div class="text-center">
                             <img class="mb-3" width="200" height="200"
                                 src="https://tecdn.b-cdn.net/img/new/standard/nature/184.jpg"/></a>
-                            <label for="">1989</label>
                         </div>
                         <div class="">
                             <h1 class="mb-2 text-center">O retorno do batman</h1>
-                            <p class="text-justify w-64">Era uma vez um menino franzino, franziiino... Que viva aos arredores de Ohio...</p>
+                            <p class="text-justify w-full">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo inventore natus rerum pariatur. Repellendus incidunt vero quod corporis minus corrupti dolorem blanditiis nostrum saepe dolores maxime repellat nam, fugit ut!</p>
                         </div>
                     </div>
                     <div class="flex sm:flex-wrap text-sm text-center justify-around gap-4 mt-5 lg:text-base">
@@ -27,6 +26,37 @@
                             <h1>Fulano de tal</h1>
                             <p>Bruce Wayne</p>
                         </div>
+                    </div>
+                    <div class="mt-5 flex flex-row items-center">
+                        <p>🇧🇪 Belgica</p>
+                    </div>
+                    <div class="mt-5 flex flex-wrap gap-10">
+                        <div class="flex flex-row items-center gap-3 bg-amber-200 border rounded-md">
+                            <img width="38" height="38" src="https://img.icons8.com/color/48/netflix-desktop-app--v1.png" alt="netflix-desktop-app--v1"/>
+                            <p class="pr-2">Netflix</p>
+                        </div>
+                        <div class="flex flex-row items-center gap-3">
+                            <img width="36" height="36" src="https://img.icons8.com/ios-filled/50/hbo-max.png" alt="hbo-max"/>
+                            <p>HBO Max</p>
+                        </div>
+                        <div class="pr-2 flex flex-row items-center gap-3 bg-amber-200 border rounded-md">
+                            <img width="40" height="40" src="https://img.icons8.com/fluency/48/amazon-prime-video.png" alt="amazon-prime-video"/>
+                            <p>Amazon Prime</p>
+                        </div>
+                        <div class="pr-2 flex flex-row items-center gap-3 bg-amber-200 border rounded-md">
+                            <img width="40" height="40" src="https://images.justwatch.com/icon/52449861/s100" alt="amazon-prime-video"/>
+                            <p>Amazon Prime</p>
+                        </div>
+                    </div>
+                    <div class="mt-10">
+                        <p>
+                            Caso queira buscar por outros países
+                        </p>
+                    </div>
+                    <div class="mt-2">
+                        <button type="button" class="border border-transparent bg-sky-500 hover:bg-sky-700 shadow-md rounded-md text-center text-sm p-2 text-white font-semibold">
+                            Busque aqui
+                        </button>
                     </div>
                 </div>
             </div>
