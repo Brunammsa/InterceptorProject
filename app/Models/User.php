0785b7566193ae60e3cf7098aff67b385a9a3685
Bruna Sa <bruna@Mac-mini-de-Bruna.local>
2023-09-21 11:47:35 -0300

    criando função de validação se é usuário api

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,9 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function isAdm()
+    {
+        return $this->name === 'api' ? true : false;
+    }
 }
