42739ef7ad9439f3c8f649fbdfb676796143afa7
Bruna Sa <bruna@Mac-mini-de-Bruna.local>
2023-09-23 15:20:25 -0300

    alteração cores layout

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,16 +2,16 @@
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
-    <div class="text-white font-semibold">
+    <div class="text-blue-jeans font-semibold">
         Login with
     </div>
     <div>
-        <a href="{{route('login.social.redirect', ['driver' => 'google'])}}" type="button" class="mt-3 bg-gray-950 text-zinc-400 hover:bg-zinc-400 hover:text-gray-950 font-semibold w-full border border-transparent rounded-md">
+        <a href="{{route('login.social.redirect', ['driver' => 'google'])}}" type="button" class="mt-3 bg-gray-modificate text-blue-jeans hover:bg-zinc-300 hover:text-gray-950 font-semibold w-full border border-1 border-blue-jeans rounded-md">
             Google 
         </a>
     </div>
     <div>
-        <a href="{{route('login.social.redirect', ['driver' => 'facebook'])}}" type="button" class="bg-gray-950 text-zinc-400 hover:bg-zinc-400 hover:text-gray-950 font-semibold mt-2 w-full border border-transparent rounded-md">
+        <a href="{{route('login.social.redirect', ['driver' => 'facebook'])}}" type="button" class="bg-gray-modificate text-blue-jeans hover:bg-zinc-300 hover:text-gray-950 font-semibold mt-2 w-full border border-1 border-blue-jeans rounded-md">
             Facebook
         </a>
     </div>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -16,10 +16,13 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center text-center items-center pt-6 sm:pt-0 bg-gradient-to-b from-zinc-500 to-zinc-900 dark:bg-gray-900">
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-zinc-800 dark:bg-gray-800 border border-zinc-800 shadow-md overflow-hidden sm:rounded-lg">
+        <div class="min-h-screen px-6 flex flex-col justify-center text-center items-center bg-navy dark:bg-gray-900">
+            <div class="w-full px-6 py-4 md:w-80 lg:w-96 bg-gray-modificate dark:bg-gray-800 border border-zinc-800 shadow-md overflow-hidden rounded-lg">
                 {{ $slot }}
             </div>
         </div>
+        <footer class="bg-yellow-lemon text-sm text-center">
+            Desenvolvido por B&B
+        </footer>
     </body>
 </html>
