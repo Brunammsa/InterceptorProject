bfe401a5bf73ef44b96da30c7fc333c35463f5d7
Bruna Sa <bruna@Mac-mini-de-Bruna.local>
2023-09-25 15:37:06 -0300

    modifcando cores layout

--- resources/views/recent-searches.blade.php
+++ resources/views/recent-searches.blade.php
@@ -1,35 +1,32 @@
 <x-app-layout>
     <div class="py-12">
         <div class="max-w-5xl mx-auto px-5 md:px-6 lg:px-8">
-            <div class="bg-zinc-300 overflow-hidden shadow-md rounded-lg">
-                <div class="p-6 text-gray-900 flex flex-col gap-2 md:flex-row md:justify-between lg:justify-between items-center">
-                    <img class="rounded-lg" src="https://tecdn.b-cdn.net/img/new/standard/nature/184.jpg"  width="200" /></a>
-                    <div class="text-center">
-                        <p>1988</p>
-                        <p>Batman</p>
+            <div class="p-6 bg-blue-jeans overflow-hidden shadow-md rounded-lg space-y-5 text-gray-modificate">
+                <div class="flex flex-col md:grid md:grid-cols-3 lg:grid-cols-3 md:gap-10 lg:gap-20 items-center">
+                    <div class="text-center md:col-span-1 lg:col-span-1">
+                        <img class="rounded-lg mb-3" width="300" height="300" src="https://tecdn.b-cdn.net/img/new/standard/nature/184.jpg"/>
                     </div>
-                    <div class="text-base text-justify flex-initial w-64">
-                        <p>Era uma vez..um menino franzino, franziiiiino.. Que morava nos arredores de Ohio..</p>
-                    </div>
-                </div>
-                <div class="p-6 text-gray-900 flex flex-col gap-2 md:flex-row md:justify-between lg:justify-between items-center">
-                    <img class="rounded-lg" src="https://tecdn.b-cdn.net/img/new/standard/nature/184.jpg"  width="200" /></a>
-                    <div class="text-center">
-                        <p>1988</p>
-                        <p>Batman</p>
-                    </div>
-                    <div class="text-base text-justify flex-initial w-64">
-                        <p>Era uma vez..um menino franzino, franziiiiino.. Que morava nos arredores de Ohio..</p>
+                    <div class="flex flex-col md:col-span-2 lg:col-span-2 items-center">
+                        <h1 class="mb-2 text-center font-semibold">O retorno do batman - 1988</h1>
+                        <p class="text-justify">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo inventore natus rerum pariatur. Repellendus incidunt vero quod corporis minus corrupti dolorem blanditiis nostrum saepe dolores maxime repellat nam, fugit ut!</p>
                     </div>
                 </div>
-                <div class="p-6 text-gray-900 flex flex-col gap-2 md:flex-row md:justify-between lg:justify-between items-center">
-                    <img class="rounded-lg" src="https://tecdn.b-cdn.net/img/new/standard/nature/184.jpg"  width="200" /></a>
-                    <div class="text-center">
-                        <p>1988</p>
-                        <p>Batman</p>
+                <div class="flex flex-col md:grid md:grid-cols-3 lg:grid-cols-3 md:gap-10 lg:gap-20 items-center">
+                    <div class="text-center md:col-span-1 lg:col-span-1">
+                        <img class="rounded-lg mb-3" width="300" height="300" src="https://tecdn.b-cdn.net/img/new/standard/nature/184.jpg"/>
                     </div>
-                    <div class="text-base text-justify flex-initial w-64">
-                        <p>Era uma vez..um menino franzino, franziiiiino.. Que morava nos arredores de Ohio..</p>
+                    <div class="flex flex-col md:col-span-2 lg:col-span-2 items-center">
+                        <h1 class="mb-2 text-center font-semibold">O retorno do batman - 1988</h1>
+                        <p class="text-justify">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo inventore natus rerum pariatur. Repellendus incidunt vero quod corporis minus corrupti dolorem blanditiis nostrum saepe dolores maxime repellat nam, fugit ut!</p>
+                    </div>
+                </div>
+                <div class="flex flex-col md:grid md:grid-cols-3 lg:grid-cols-3 md:gap-10 lg:gap-20 items-center">
+                    <div class="text-center md:col-span-1 lg:col-span-1">
+                        <img class="rounded-lg mb-3" width="300" height="300" src="https://tecdn.b-cdn.net/img/new/standard/nature/184.jpg"/>
+                    </div>
+                    <div class="flex flex-col md:col-span-2 lg:col-span-2 items-center">
+                        <h1 class="mb-2 text-center font-semibold">O retorno do batman - 1988</h1>
+                        <p class="text-justify">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo inventore natus rerum pariatur. Repellendus incidunt vero quod corporis minus corrupti dolorem blanditiis nostrum saepe dolores maxime repellat nam, fugit ut!</p>
                     </div>
                 </div>
             </div>
