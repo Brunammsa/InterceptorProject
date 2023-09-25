13b373f00910919a1b26e196c22935c250e4f120
Bruna Sa <bruna@Mac-mini-de-Bruna.local>
2023-09-25 14:36:56 -0300

    modifcando cores layout

--- resources/views/index-movie.blade.php
+++ resources/views/index-movie.blade.php
@@ -3,17 +3,17 @@
         <div class=" max-w-5xl mx-auto px-5 md:px-6 lg:px-8">
             <div class="bg-gray-modificate overflow-hidden shadow-md rounded-lg">
                 <div class="p-6 text-blue-jeans">
-                    <div class="flex flex-col md:flex-row lg:flex-row md:gap-10 lg:gap-20 items-center">
+                    <div class="flex flex-col md:flex-row grid md:grid-cols-3 lg:grid-cols-3 md:gap-10 lg:gap-20 items-center">
                         <div class="text-center">
                             <img class="rounded-lg mb-3" width="300" height="300"
                                 src="https://tecdn.b-cdn.net/img/new/standard/nature/184.jpg"/></a>
                         </div>
-                        <div class="flex flex-col items-center">
+                        <div class="flex flex-col md:col-span-2 lg:col-span-2 items-center">
                             <h1 class="mb-2 text-center">O retorno do batman</h1>
-                            <p class="text-justify flex-auto w-5/6">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo inventore natus rerum pariatur. Repellendus incidunt vero quod corporis minus corrupti dolorem blanditiis nostrum saepe dolores maxime repellat nam, fugit ut!</p>
+                            <p class="text-justify">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo inventore natus rerum pariatur. Repellendus incidunt vero quod corporis minus corrupti dolorem blanditiis nostrum saepe dolores maxime repellat nam, fugit ut!</p>
                         </div>
                     </div>
-                    <div class="flex sm:flex-wrap text-sm text-center justify-around gap-4 mt-5 lg:text-base">
+                    <div class="flex sm:flex-wrap text-sm text-center justify-around gap-4 mt-10 lg:text-base">
                         <div>
                             <h1>Fulano de tal</h1>
                             <p>Bruce Wayne</p>
@@ -30,20 +30,20 @@
                     <div class="mt-5 flex flex-row items-center">
                         <p>🇧🇪 Belgica</p>
                     </div>
-                    <div class="mt-5 flex flex-col md:flex-wrap lg:flex-row gap-5">
-                        <div class="flex flex-row items-center gap-3 bg-amber-200 border rounded-md">
+                    <div class="mt-5 flex flex-col md:flex-row lg:flex-row gap-5">
+                        <div class="flex flex-row items-center m-2 gap-3 bg-amber-200 border rounded-md">
                             <img width="38" height="38" src="https://img.icons8.com/color/48/netflix-desktop-app--v1.png" alt="netflix-desktop-app--v1"/>
                             <p class="pr-2">Netflix</p>
                         </div>
-                        <div class="flex flex-row items-center gap-3">
+                        <div class="flex flex-row items-center m-2 gap-3">
                             <img width="36" height="36" src="https://img.icons8.com/ios-filled/50/hbo-max.png" alt="hbo-max"/>
                             <p>HBO Max</p>
                         </div>
-                        <div class="pr-2 flex flex-row items-center gap-3 bg-amber-200 border rounded-md">
+                        <div class="pr-2 flex flex-row items-center m-2 gap-3 bg-amber-200 border rounded-md">
                             <img width="40" height="40" src="https://img.icons8.com/fluency/48/amazon-prime-video.png" alt="amazon-prime-video"/>
                             <p>Amazon Prime</p>
                         </div>
-                        <div class="pr-2 flex flex-row items-center gap-3 bg-amber-200 border rounded-md">
+                        <div class="pr-2 flex flex-row items-center m-2 gap-3 bg-amber-200 border rounded-md">
                             <img width="40" height="40" src="https://images.justwatch.com/icon/52449861/s100" alt="amazon-prime-video"/>
                             <p>Amazon Prime</p>
                         </div>
